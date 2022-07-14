71e9ba2b654e1c8eada2dc3dbc1eb00df63ebdb2
Jonathon <jwbialas@gmail.com>
2022-07-14 03:46:09 -0500

    commit tree 01130d24c7041662882266eb5f470aa6c7710fb2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function createEmployeeRecord(Array){
+    let testEmployee = {}
+    testEmployee.firstName = Array[0]
+    testEmployee.familyName = Array[1]
+    testEmployee.title = Array[2]
+    testEmployee.payPerHour = Array[3]
+    testEmployee.timeInEvents = []
+    testEmployee.timeOutEvents = []
+    return testEmployee
+    
+}
+
+function createEmployeeRecords(){
+    let employeeRecords = [[],[]]
+
+}
+
+function createTimeInEvent(){
+
+}
+
+function createTimeOutEvent(){
+
+}
+
